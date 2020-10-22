a829b320233641040b49e82b5c69952293f86ee9
Søren Lyster <soren@vogglyster.dk>
2020-10-22 16:52:04 +0200

    Add getMagnitudes func and test

---
+++ test/test_getMagnitudes.m
@@ -0,0 +1,16 @@
+%% Test getMagnitudes function
+
+% % Test data ------------------ %
+[audio, fs] = audioread("Snare.wav");
+audio = audio(:,1);
+% % ---------------------------- %
+
+magnitudes = getMagnitudes(audio, 1024);
+
+k = 1;
+[rows, coloumns] = size(magnitudes);
+for n = 1:1:coloumns
+    plot(magnitudes(:,n),'color',rand(1,3))
+    hold on
+    k = k+1
+end
